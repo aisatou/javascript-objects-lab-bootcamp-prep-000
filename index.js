040a611bcc2762a6114900c50686d3ec0fe78bdd
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-18 04:08:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var recipes = { a: 1 };
 
-// function updateObjectWithKeyAndValue(object, key, value) {
-//   recipes[key] = value;
-//   return recipes;
-// }
-
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
