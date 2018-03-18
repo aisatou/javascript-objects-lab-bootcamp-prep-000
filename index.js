e064173f9edab044277cd599e83bfef66f1667ee
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-18 04:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = { a: 1 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = Object.assign
-  // object[key] = value;
-  // return object;
+  var newObj = Object.assign({}, object);
+  newObj[key] = value;
+  return object;
 }
